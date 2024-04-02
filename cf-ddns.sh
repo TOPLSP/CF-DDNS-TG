a361849bf84d46f9fc9338278da5f9ab02d9b507
TOPLSP <38609188+TOPLSP@users.noreply.github.com>
2024-04-02 22:55:53 +0800

    Add files via upload

---
+++ cf-ddns.sh
@@ -0,0 +1,148 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+# Automatically update your CloudFlare DNS record to the IP, Dynamic DNS
+# Can retrieve cloudflare Domain id and list zone's, because, lazy
+# Place at:
+# curl https://raw.githubusercontent.com/yulewang/cloudflare-api-v4-ddns/master/cf-v4-ddns.sh > /usr/local/bin/cf-ddns.sh && chmod +x /usr/local/bin/cf-ddns.sh
+# run `crontab -e` and add next line:
+# */1 * * * * /usr/local/bin/cf-ddns.sh >/dev/null 2>&1
+# or you need log:
+# */1 * * * * /usr/local/bin/cf-ddns.sh >> /var/log/cf-ddns.log 2>&1
+
+# Usage:
+# cf-ddns.sh -k cloudflare-api-key \
+#            -u user@example.com \
+#            -h host.example.com \     # fqdn of the record you want to update
+#            -z example.com \          # will show you all zones if forgot, but you need this
+#            -t A|AAAA                 # specify ipv4/ipv6, default: ipv4
+#            -f false|true \           # force dns update, disregard local stored ip
+
+# default config
+# API key, see https://www.cloudflare.com/a/account/my-account,
+# incorrect api-key results in E_UNAUTH error -    填写api-key
+CFKEY=
+
+# Username, eg: user@example.com 填写登录邮箱
+CFUSER=
+
+# Zone name, eg: example.com  填写域名
+CFZONE_NAME=
+
+# Hostname to update, eg: homeserver.example.com 填写 主机名
+CFRECORD_NAME=
+
+# Record type, A(IPv4)|AAAA(IPv6), default IPv4
+CFRECORD_TYPE=A
+# Cloudflare TTL for record, between 120 and 86400 seconds
+CFTTL=1
+# Ignore local file, update ip anyway
+FORCE=false
+
+# TGbot设置                                填写
+API_TOKEN=""
+CHAT_ID=""
+# 发送内容
+MESSAGE="[xxx]IP已更新为 $WAN_IP"
+
+WANIPSITE="http://ipv4.icanhazip.com"
+# Site to retrieve WAN ip, other examples are: bot.whatismyipaddress.com, https://api.ipify.org/ ...
+if [ "$CFRECORD_TYPE" = "A" ]; then
+  :
+elif [ "$CFRECORD_TYPE" = "AAAA" ]; then
+  WANIPSITE="http://ipv6.icanhazip.com"
+else
+  echo "$CFRECORD_TYPE specified is invalid, CFRECORD_TYPE can only be A(for IPv4)|AAAA(for IPv6)"
+  exit 2
+fi
+
+# get parameter
+while getopts k:u:h:z:t:f: opts; do
+  case ${opts} in
+    k) CFKEY=${OPTARG} ;;
+    u) CFUSER=${OPTARG} ;;
+    h) CFRECORD_NAME=${OPTARG} ;;
+    z) CFZONE_NAME=${OPTARG} ;;
+    t) CFRECORD_TYPE=${OPTARG} ;;
+    f) FORCE=${OPTARG} ;;
+  esac
+done
+
+# If required settings are missing just exit
+if [ "$CFKEY" = "" ]; then
+  echo "缺少api密钥，请访问: https://www.cloudflare.com/a/account/my-account 获取,并编辑配置或使用-k xxxxx 设置"
+  exit 2
+fi
+if [ "$CFUSER" = "" ]; then
+  echo "缺少用户名,请填写登录邮箱"
+  exit 2
+fi
+if [ "$CFRECORD_NAME" = "" ]; then 
+  echo "缺少主机名，请编辑配置或使用-h xxx.xxx.com 设置"
+  exit 2
+fi
+
+# If the hostname is not a FQDN
+if [ "$CFRECORD_NAME" != "$CFZONE_NAME" ] && ! [ -z "${CFRECORD_NAME##*$CFZONE_NAME}" ]; then
+  CFRECORD_NAME="$CFRECORD_NAME.$CFZONE_NAME"
+  echo " => 主机名不完整,设为$CFRECORD_NAME"
+fi
+
+# Get current and old WAN ip
+WAN_IP=`curl -s ${WANIPSITE}`
+WAN_IP_FILE=$HOME/.cf-wan_ip.txt
+if [ -f $WAN_IP_FILE ]; then
+  OLD_WAN_IP=`cat $WAN_IP_FILE`
+else
+  echo "IP未变更"
+  OLD_WAN_IP=""
+fi
+
+# If WAN IP is unchanged an not -f flag, exit here
+if [ "$WAN_IP" = "$OLD_WAN_IP" ] && [ "$FORCE" = false ]; then
+  echo "IP未变更, 强制更新ip请使用 -f true"
+  exit 0
+fi
+
+# Get zone_identifier & record_identifier
+ID_FILE=$HOME/.cf-id_$CFRECORD_NAME.txt
+if [ -f $ID_FILE ] && [ $(wc -l $ID_FILE | cut -d " " -f 1) == 4 ] \
+  && [ "$(sed -n '3,1p' "$ID_FILE")" == "$CFZONE_NAME" ] \
+  && [ "$(sed -n '4,1p' "$ID_FILE")" == "$CFRECORD_NAME" ]; then
+    CFZONE_ID=$(sed -n '1,1p' "$ID_FILE")
+    CFRECORD_ID=$(sed -n '2,1p' "$ID_FILE")
+else
+    echo "正在更新主机域名标识"
+    CFZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$CFZONE_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
+    CFRECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records?name=$CFRECORD_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*' | head -1 )
+    echo "$CFZONE_ID" > $ID_FILE
+    echo "$CFRECORD_ID" >> $ID_FILE
+    echo "$CFZONE_NAME" >> $ID_FILE
+    echo "$CFRECORD_NAME" >> $ID_FILE
+fi
+
+# If WAN is changed, update cloudflare
+echo "DNS更新为 $WAN_IP"
+RESPONSE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records/$CFRECORD_ID" \
+  -H "X-Auth-Email: $CFUSER" \
+  -H "X-Auth-Key: $CFKEY" \
+  -H "Content-Type: application/json" \
+  --data "{\"id\":\"$CFZONE_ID\",\"type\":\"$CFRECORD_TYPE\",\"name\":\"$CFRECORD_NAME\",\"content\":\"$WAN_IP\", \"ttl\":$CFTTL}")
+  
+if [ "$API_TOKEN" = "" ] && [ "$CHAT_ID" = "" ]; then
+  echo "TGBOT未配置"
+  exit 2
+fi
+
+if [ "$RESPONSE" != "${RESPONSE%success*}" ] && [ "$(echo $RESPONSE | grep "\"success\":true")" != "" ]; then
+  echo "更新完成!"
+  echo $WAN_IP > $WAN_IP_FILE
+  # 向Telegram Bot发送消息
+  curl -s -X POST https://api.telegram.org/bot$API_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE"
+  exit
+else
+  echo '出现错误 :('
+  echo "响应: $RESPONSE"
+  exit 1
+fi
